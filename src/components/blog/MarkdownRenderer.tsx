71fbafc9fec8880cb8c06584c60ea7675f37c2b7
Amir Mohammad Yousefvand <amiroooyousefi@gmail.com>
2026-09-03 20:33:36 +0330

    Fix syntax error in MarkdownRenderer without bash interpretation issues

--- src/components/blog/MarkdownRenderer.tsx
+++ src/components/blog/MarkdownRenderer.tsx
@@ -13,12 +13,11 @@ export default function MarkdownRenderer({
 
   // 0. Remove Tiptap's markdown hard breaks (\ followed by newline)
   //    and any stray backslashes right before a heading
-  cleanContent = cleanContent.replace(/\\n/g, "\n");
+  cleanContent = cleanContent.replace(/\\\n/g, "\n");
 
   // 1. Separate headings that are stuck to the end of another element on the same line
   // Example: `![alt](url)### Heading` -> `![alt](url)\n\n### Heading`
-  // We exclude `\` from $1 so we don't accidentally preserve a stray escape char
-  cleanContent = cleanContent.replace(/([^\n\s\xA0​‍﻿\])[\s\xA0​‍﻿\]*(#{1,6})(?=\s|[a-zA-Z0-9])/g, "$1\n\n$2");
+  cleanContent = cleanContent.replace(/([^\n\s\xA0​‍﻿])[\s\xA0​‍﻿]*(#{1,6})/g, "$1\n\n$2");
 
   // 2. Remove spaces and zero-width characters at the START of a line before a hash
   cleanContent = cleanContent.replace(/^[\s\xA0​‍﻿]+(#{1,6})/gm, "$1");
